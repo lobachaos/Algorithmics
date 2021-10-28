cd834213f55185d7514c2eedc8bd951669104767
lobachaos <zwolfsasa@gmail.com>
2021-10-28 08:57:13 -0400

    MaxPairwiseProduct

--- src/MaxPairwiseProduct.java
+++ src/MaxPairwiseProduct.java
@@ -2,22 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class MaxPairwiseProduct {
+    static long getMaxPairwiseProduct(int[] numbers) {
+        long max_product;
+        int max_index = -1;
 
-
-    static int getMaxPairwiseProduct(int[] numbers) {
-        int product = 0;
-        int n = numbers.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                product = Math.max(product,
-                        numbers[i] * numbers[j]);
+        for (int i = 0; i < numbers.length; i++) {
+            if (max_index == -1 || numbers[i] > numbers[max_index]) {
+                max_index = i;
             }
         }
-        return product;
+
+        int max_index2 = -1;
+        for (int j = 0; j < numbers.length; j++) {
+            if ((j != max_index) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+                max_index2 = j;
+        }
+
+
+        max_product = (long) numbers[max_index] * numbers[max_index2];
+
+        return max_product;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
     }
 
     static class FastScanner {
         BufferedReader br;
+        StringTokenizer st;
 
         FastScanner(InputStream stream) {
             try {
@@ -29,7 +48,6 @@ public class MaxPairwiseProduct {
         }
 
         String next() {
-            StringTokenizer st = null;
             while (st == null || !st.hasMoreTokens()) {
                 try {
                     st = new StringTokenizer(br.readLine());
@@ -45,16 +63,4 @@ public class MaxPairwiseProduct {
         }
     }
 
-    public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        System.out.println(getMaxPairwiseProduct(numbers));
-    }
-
-
 }
-
